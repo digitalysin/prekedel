2ed323a0b4afe246732d455aad46ffb08bc24b9b
digitalysin <digitalysin@yahoo.co.id>
2012-10-04 18:53:51 +0700

    change unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,18 @@
+# server_path = "/var/server/online-shop/current"
+# working_directory server_path
+# pid "#{server_path}/tmp/pids/unicorn.pid"
+# stderr_path "#{server_path}/log/unicorn-error.log"
+# stdout_path "#{server_path}/log/unicorn.log"
+# listen "#{server_path}/tmp/unicorn.sock"
+# worker_processes 3
+# timeout 60
+
+
 server_path = "/var/server/online-shop/current"
 working_directory server_path
-pid "#{server_path}/tmp/pids/unicorn.pid"
-stderr_path "#{server_path}/log/unicorn-error.log"
-stdout_path "#{server_path}/log/unicorn.log"
-listen "#{server_path}/tmp/unicorn.sock"
+pid "/tmp/unicorn.pid"
+stderr_path "/tmp/log/unicorn-error.log"
+stdout_path "/tmp/log/unicorn.log"
+listen "/tmp/unicorn.sock"
 worker_processes 3
 timeout 60
