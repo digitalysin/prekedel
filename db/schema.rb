dbf5b178f99cf696ad8eb4b7b63835cc37edc8df
digitalysin <digitalysin@yahoo.co.id>
2012-10-07 09:51:03 +0700

    change migration script

--- db/schema.rb
+++ db/schema.rb
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(:version => 20121006090233) do
     t.string   "namespace"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+  # add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  # add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  # add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
 
   create_table "admin_users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -43,8 +43,8 @@ ActiveRecord::Schema.define(:version => 20121006090233) do
     t.datetime "updated_at",                             :null => false
   end
 
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+  # add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  # add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "avatars", :force => true do |t|
     t.string   "image"
